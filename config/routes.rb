112b85debfbe5734714d37fd4800936077361772
ychen21 <ychen21@academic.rrc.ca>
2025-11-23 10:46:10 -0600

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  ActiveAdmin.routes(self)
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root "products#index"
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resource :cart, only: [:show] do
+    post "add/:product_id", to: "carts#add", as: :add
+    patch "update/:product_id", to: "carts#update", as: :update
+    delete "remove/:product_id", to: "carts#remove", as: :remove
+  end
+
+  resources :orders, only: [:index, :show, :create]
+  get "/checkout", to: "checkout#new"
+  post "/checkout", to: "checkout#create"
+
+  get "/pages/:title", to: "pages#show", as: :page
 end
